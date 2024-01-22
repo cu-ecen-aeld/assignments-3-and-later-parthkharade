2a038e69ee9a928a565a7a121fa8cae2e54b5e26
Parth Kharade <parthkharade99@gmail.com>
2024-01-22 16:25:54 +0000

    Added comments to files.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Global variables to retain value between recursive calls.
 filecount=0
 matchcount=0
 maindir=$1
 searchstr=$2
+# Search function performs a recursive search on the directory structure counting files and looking for matches.
 search(){
 	for file in $1/*
 	do
@@ -19,16 +21,18 @@ search(){
 	done
 }
 
-
+# Check if the number of arguments are not equal to 2. Throw an error in that case.
 if [ $# -ne 2 ]
 then
 	echo Invalid number of inputs.
 	echo command format : ./finder.sh directory search_str
 	exit  1
+# Check if the first argument is a valid directory
 elif [ ! -d $1 ]
 then
 	echo The first argument must be a directory.
 	exit 1
+# If All checks pass then perform the search and match operation
 else
 	search $1 $2
 	echo The number of files are $filecount and the number of matching lines are $matchcount
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+# Check if the correct number of arguments are present.
 if [ $# -ne 2 ]
 then
 	echo Invalid number of arguments.
 	echo command format : ./writer.sh path_to_file string_to_be_insterted
 	exit 1
+# If inputs are valid then create and write to the required file.
 else
 	mkdir -p "$(dirname "$1")" && touch "$1"
 	echo $2 > $1
