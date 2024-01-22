3573de4856a1d09813e7370c9b56796d8fc1adf2
Parth Kharade <parthkharade99@gmail.com>
2024-01-22 15:49:03 +0000

    Added exit code to writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,6 +4,7 @@ if [ $# -ne 2 ]
 then
 	echo Invalid number of arguments.
 	echo command format : ./writer.sh path_to_file string_to_be_insterted
+	exit 1
 else
 	mkdir -p "$(dirname "$1")" && touch "$1"
 	echo $2 > $1
